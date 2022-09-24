1be17b3b687fef982d73345ce94c583089d06821
The One and Only Uper <111448336+UperscuzziSchoolAcc@users.noreply.github.com>
2022-09-24 13:35:52 -0500

    Redid the whole script, added operators and bug fixes

--- kidsLang.py
+++ kidsLang.py
@@ -1,79 +1,121 @@
 """
 KidsLang Interpreter, made by Greyson Rowland (2022)
 """
-import time
+
+import re, time
+
+error = False
+mainLoop = 0
 f = open("myCode.kl")
 lines = f.readlines()
-i = 0
 variables = {}
-def incrementI(n):
-    global i
-    i += int(n)
-def ifPartInString(part, string):
-    for i in range(len(string)):
-        if string[i: len(part)] == part:
-            return True
-    return False
-def sayStringWithout(part, string):
-    if ifPartInString(part, string):
-        return string[len(part):len(string)]
-def getWord(word, string):
-    return string.split()[word - 1]
-def getRepeatLines(line):
-    a = []
-    iter = 1
-    while not ifPartInString("end" ,lines[line + iter]):
-        curr = lines[line + iter]
-        a.append(curr.strip())
-        iter += 1
-        if iter == len(lines):
-            return "error"
-    return a
-def convertVariables(command):
-    if len(variables) != 0:
-        spread = command.split()
-        for a in range(len(spread)):
-            for b in range(len(variables)):
-                if list(variables.keys())[b] == spread[a]:
-                    spread[a] = variables[spread[a]]
-        return " ".join(spread)
-    else:
-        return command
-def execute(command, loop, exec):
-    command = command.strip()
-    if command.strip()[0] != "make":
-        command = convertVariables(command)
-    if command[0:4] == "say ":
-        if exec == True:
-            print(sayStringWithout("say ", command))
-    elif command[0:7] == "repeat ":
-        if exec == True:
-            rpt = getRepeatLines(loop)
-            for a in range(int(sayStringWithout("repeat ", command))):
-                for b in range(len(rpt)):
-                    execute(rpt[b], loop + (b - 1), True)
-            incrementI(len(rpt))
-    elif command[0:3] == "end":
-        pass
-    elif command[0:2] == "--":
-        pass
-    elif command[0:5] == "make ":
-        if exec == True:
-            varName = getWord(2, command)
-            varValue = command[5 + len(varName) + 1:len(command)]
-            variables[str(varName)] = varValue
-    elif command[0:5] == "wait ":
-        sl = command.split()[1]
-        time.sleep(int(sl))
-    else:
-        print("Error on line " + str(loop + 1) + ", did you spell something wrong?")
-        return "error"
 
-#####################################################
-        
-while i < len(lines):
-    if execute(lines[i], i, False) != "error":
-        execute(lines[i], i, True)
+def throwError(statement):
+    global error
+    error = True
+    print("Error on line",str(mainLoop + 1) + ":",statement)
+    
+def computeOperations(command):
+    spl = command.split()
+    locLoop = 0
+    while locLoop < len(spl):
+        op = spl[locLoop]
+        if op == "+":
+            try:
+                ta = str(int(spl[locLoop - 1]) + int(spl[locLoop + 1]))
+                spl[locLoop - 1] = ta
+                spl.pop(locLoop)
+                spl.pop(locLoop)
+                locLoop = 0
+            except:
+                throwError("You can't add a letter with a number.")
+        if op == "-":
+            try:
+                ta = str(int(spl[locLoop - 1]) - int(spl[locLoop + 1]))
+                spl[locLoop - 1] = ta
+                spl.pop(locLoop)
+                spl.pop(locLoop)
+                locLoop = 0
+            except:
+                throwError("You can't subtract a letter with a number.")
+        if op == "*":
+            try:
+                ta = str(int(spl[locLoop - 1]) * int(spl[locLoop + 1]))
+                spl[locLoop - 1] = ta
+                spl.pop(locLoop)
+                spl.pop(locLoop)
+                locLoop = 0
+            except:
+                throwError("You can't multiply a letter with a number.")
+        if op == "/":
+            try:
+                ta = str(int(spl[locLoop - 1]) / int(spl[locLoop + 1]))
+                spl[locLoop - 1] = ta
+                spl.pop(locLoop)
+                spl.pop(locLoop)
+                locLoop = 0
+            except:
+                throwError("You can't divide a letter with a number.")
+        locLoop += 1
+    return " ".join(spl)
+
+def getLoopParts(loop):
+    global mainLoop
+    toRet = []
+    i = loop + 1
+    while lines[i - 1] != "end":
+        toRet.append(lines[i].strip())
+        i += 1
+        mainLoop += len(toRet)
+    toRet.pop()
+    return toRet
+
+def convertVariables(command):
+    a = command.split()
+    for i in range(len(a)):
+        if variables.get(a[i]) != None:
+            a[i] = variables.get(a[i])
+    return " ".join(a)
+
+def execute(command, loop, run):
+    ss = command.split()
+    command = command.strip()
+    if ss[0] != "make":
+        command = convertVariables(command)
     else:
+        FH = ss[0:2]
+        SH = convertVariables(" ".join(ss[2:len(ss)]))
+        command = " ".join(FH) + " " + "".join(SH)
+    command = computeOperations(command)
+    ss = command.split()
+    s = re.split(r'(\s+)', command)
+    if s[0] == "say":
+        if run == True:
+            print("".join(s[1:len(s)]))
+    elif s[0] == "--":
+        if run == True:
+            pass
+    elif s[0] == "make":
+        if run == True:
+            variables[ss[1]] = "".join(s[3:len(s)]).strip()
+    elif s[0] == "repeat":
+        if run == True:
+            toRep = getLoopParts(mainLoop)
+            for a in range(int(ss[1])):
+                for b in range(len(toRep)):
+                    execute(toRep[b], loop, True)
+    elif s[0] == "wait":
+        if run == True:
+            time.wait(int(s[1]))
+    else:
+         return "error"   
+    
+        
+while mainLoop < len(lines):
+    if error:
         break
-    i += 1
+    command = lines[mainLoop]
+    if execute(command, mainLoop, False):
+        break
+    execute(command, mainLoop, True)
+    mainLoop += 1
